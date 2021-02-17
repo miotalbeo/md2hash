0dcf427aaf8555cc916c4dfd19466300472ec08d
a <no@no.com>
2021-02-17 01:51:57 +0000

    updated hashing function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::env::args;
-use std::process::exit;
+use std::str;
 
 const PI_SUBST: [u8; 256] = [
-    1, 46, 67, 201, 162, 216, 124, 1, 61, 54, 84, 161, 236, 240, 6,
+    41, 46, 67, 201, 162, 216, 124, 1, 61, 54, 84, 161, 236, 240, 6,
     19, 98, 167, 5, 243, 192, 199, 115, 140, 152, 147, 43, 217, 188,
     76, 130, 202, 30, 155, 87, 60, 253, 212, 224, 22, 103, 66, 111, 24,
     138, 23, 229, 18, 190, 78, 196, 214, 218, 158, 222, 73, 160, 251,
@@ -23,26 +22,16 @@ const PI_SUBST: [u8; 256] = [
 ];
 
 fn main() {
-    let args: Vec<String> = args().collect();
-    // collect command-line arguments into a vector
-    if args.len() < 3 {
-        exit(1);
-    }
-    // exit if number of arguments is less than 3
-
-    let f_option = args[1] == "f";
-    let s_option = args[1] == "s";
-
-    if !f_option && !s_option {
-        println!("Invalid switch: {}", args[1]);
-        exit(1);
-    }
-
     let l = pad_to_16("hello".to_string().into_bytes());
-    println!("{:?}", l);
-
     let l = append_checksum(l);
-    println!("{:?}", l);
+    let l = calculate_hash(l);
+
+    let l = format!("{:02x?}", l);
+    let l = l.replace("[", "");
+    let l = l.replace("]", "");
+    let l = l.replace(",", "");
+    let l = l.replace(" ", "");
+    println!("{}", l);
 }
 
 fn pad_to_16(s: Vec<u8>) -> Vec<u8> {
@@ -50,7 +39,7 @@ fn pad_to_16(s: Vec<u8>) -> Vec<u8> {
     let s_len = s.len();
     let padding_len = 16 - (s_len % 16);
 
-    for i in 0..padding_len {
+    for _i in 0..padding_len {
         output_vec.push(padding_len as u8)
     }
     output_vec
@@ -65,7 +54,7 @@ fn append_checksum(m: Vec<u8>) -> Vec<u8> {
     // For i = 0 to 15 do
     //  Set C[i] to 0
     // end
-    let mut checksum = vec![0 as u8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let mut checksum: Vec<u8> = vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     let n = m.len();
     // Set L to 0.
     let mut l = 0;
@@ -78,11 +67,41 @@ fn append_checksum(m: Vec<u8>) -> Vec<u8> {
             // Set c to M[i*16 + j]
             let c = m[(i*16) + j];
             // Set C[j] to S[c xor l]
-            checksum[j] = PI_SUBST[c as usize ^ l as usize];
+            checksum[j] = PI_SUBST[(c ^ l) as usize];
             // Set L to C[j]
             l = checksum[j];
         }
     }
     output_vec.append(&mut checksum);
     output_vec
+}
+
+fn calculate_hash(m: Vec<u8>) -> Vec<u8> {
+    let mut x = vec![
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    ];
+
+    let n = m.len();
+    for i in 0..n/16 {
+        for j in 0..16 {
+            x[16 + j] = m[i*16 + j];
+            x[32 + j] = x[16 + j] ^ x[j];
+        }
+
+        let mut t: usize = 0;
+
+        for j in 0..18 {
+            for k in 0..48 {
+                t = (x[k] ^ PI_SUBST[t as usize]) as usize;
+                x[k] = x[k] ^ PI_SUBST[t as usize];
+            }
+
+            t = (t + j) % 256
+        }
+    }
+
+    let output = x[0..16].to_vec();
+    output
 }
