c494155a5e68882b6a24a4ea51f60a63895716e5
a <no@no.com>
2021-02-17 22:42:43 +0000

    update utility.rs

--- src/utility.rs
+++ src/utility.rs
@@ -1,3 +1,8 @@
+use std::fs::File;
+use std::io::Read;
+
+mod md2;
+
 pub fn usage_no_args(binary: &String) {
     println!("Usage: {} f/s [filename or string]", binary);
 }
@@ -10,4 +15,29 @@ pub fn usage_with_switch(binary: &String, switch: &String) {
     } else {
         usage_no_args(binary);
     }
+}
+
+pub fn hash_file(filename: String) {
+    let file_contents = read_file(filename);
+    let hashed_contents = md2::hash(file_contents);
+
+    let string = format!("{:02x?}", hashed_contents);
+    let string = string.replace("[", "");
+    let string = string.replace("]", "");
+    let string = string.replace(",", "");
+    let string = string.replace(" ", "");
+
+    println!("{}", string);
+}
+
+pub fn hash_string(strings: Vec<String>) {
+    
+}
+
+fn read_file(filename: String) -> Vec<u8> {
+    let mut f = File::open().expect("Invalid filename");
+    let metadata = fs::metadata(&filename).expect("Unable to read metadata");
+    let mut buffer = vec![0; metadata.len() as usize];
+    f.read(&mut buffer).expect("Buffer overflow");
+    buffer
 }
